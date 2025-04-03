6f2ff5fdaca4616798fbe16fd8d94fdffec07028
aryan7122 <aryankushwahalifenote@gmail.com>
2025-04-03 23:23:40 +0530

    seo done in home

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build" // ✅ Isko "dist" se "build" kar do
-  },
-  
  
 })
