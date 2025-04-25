701e6ab72f3bde2d94101b98627149c11761cce3
aryan7122 <aryankushwahalifenote@gmail.com>
2025-04-25 11:40:50 +0530

    last src

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -13,8 +13,8 @@ const SEO = ({ documentProps }) => {
 
     const siteUrl = import.meta.env.VITE_SITE_URL || window.location.origin;
     const fullCanonical = `${siteUrl}${canonicalPath}` || window.location.href;
-    const ogImageUrl = ogImage ; //? `${siteUrl}/${ogImage}` : ''; // `${siteUrl}/default-image.jpg`;
-    console.log('ogImageUrl', ogImage)
+    const ogImageUrl = ogImage ? `${siteUrl}/${ogImage}` :  'https://growththeories.vercel.app/assets/rocket-g1xUn_Ej.png'; // `${siteUrl}/default-image.jpg`;
+    console.log('ogImageUrl', ogImageUrl)
     return (
         <Helmet>
             <title>{title}</title>
